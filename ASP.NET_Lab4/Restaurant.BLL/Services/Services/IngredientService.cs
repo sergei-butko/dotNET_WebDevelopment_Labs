39b72fe1b9b9ac70e80ae1769df1e773b1ccae34
Sergei Butko <s.butko@itransition.com>
2021-12-20 20:04:07 +0200

    Lab_4 - Rewrite for MVC with Razor Pages

---
+++ ASP.NET_Lab4/Restaurant.BLL/Services/Services/IngredientService.cs
@@ -0,0 +1,26 @@
+﻿using Restaurant.BLL.Repositories.Interfaces;
+using Restaurant.BLL.Services.Interfaces;
+using Restaurant.DAL.Models;
+
+namespace Restaurant.BLL.Services.Services
+{
+    public class IngredientService : IIngredientService
+    {
+        private readonly IGenericRepository<Ingredient> _ingredientRepository;
+
+        public IngredientService(IGenericRepository<Ingredient> ingredientRepository)
+        {
+            _ingredientRepository = ingredientRepository;
+        }
+
+        public IEnumerable<Ingredient> GetAllIngredients()
+        {
+            return _ingredientRepository.GetAll();
+        }
+
+        public Ingredient GetIngredient(int ingredientId)
+        {
+            return _ingredientRepository.GetById(ingredientId);
+        }
+    }
+}
